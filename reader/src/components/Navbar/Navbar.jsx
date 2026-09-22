f215d4c7011ae1bb4791d59cd13f588b22bab6a2
claudiasrn <perezserranoclaudia@gmail.com>
2026-09-23 00:39:30 +0200

    feat: show auth state in navbar

--- reader/src/components/Navbar/Navbar.jsx
+++ reader/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router";
+import { useAuth } from "../../context/useAuth";
 
 export default function Navbar() {
+	const { user, loading, logout } = useAuth();
+
 	return (
 		<header>
 			<Link to="/">Umweg</Link>
 			<nav>
-				<Link to="/login">Log in</Link>
-				<Link to="/signup">Sign up</Link>
+				{loading ? null : user ? (
+					<>
+						<span>{user.username}</span>
+						<button onClick={logout}>Log out</button>
+					</>
+				) : (
+					<>
+						<Link to="/login">Log in</Link>
+						<Link to="/signup">Sign up</Link>
+					</>
+				)}
 			</nav>
 		</header>
 	);
